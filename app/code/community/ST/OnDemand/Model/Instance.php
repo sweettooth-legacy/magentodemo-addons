d5f020483f905a8b8ea20d9e13689f0653d7caf6
Romeo Cozac <romeo_cozac@yahoo.com>
2016-02-16 18:48:14 +0200

    Moving ST_Automation to new repo and renaming TBT_OnDemand to ST_OnDemand

--- app/code/community/ST/OnDemand/Model/Instance.php
+++ app/code/community/ST/OnDemand/Model/Instance.php
@@ -1,15 +1,15 @@
 <?php
 
-class TBT_OnDemand_Model_Instance extends Mage_Core_Model_Abstract
+class ST_OnDemand_Model_Instance extends Mage_Core_Model_Abstract
 {
     public function _construct()
     {
-        $this->_init('tbt_ondemand/instance');
+        $this->_init('st_ondemand/instance');
     }
     
     /**
      * Load the current instance
-     * @return TBT_OnDemand_Model_Instance
+     * @return ST_OnDemand_Model_Instance
      */
     public function loadCurrentInstance()
     {
--- app/code/community/TBT/OnDemand/Model/Resource/Setup.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class TBT_OnDemand_Model_Resource_Setup extends Mage_Core_Model_Resource_Setup 
-{
-
-}
